56eec4676bf9bf318176ed84d55af0ce5a2520f6
Ishmam <mdishmam2003@gmail.com>
2025-07-07 19:59:49 +0600

    update

--- method1.py
+++ method1.py
@@ -1,21 +1,27 @@
 # Method 1
-
 import tkinter as tk
 from PIL import Image, ImageTk
 def resize_image(event):
-    # Get the new size of the frame
-    new_width = event.width
+    # Get the new size of the frame + adjustment
+    new_width = event.height
     new_height = event.height
+    try:
+        resample = Image.Resampling.LANCZOS
+    except AttributeError:
+        resample = Image.ANTIALIAS
 
     # Resize the original image to the new size
-    resized = original_image.resize((new_width, new_height), Image.ANTIALIAS)
-
+    resized_bau = bau_logo.resize((new_width+10, new_height-10),resample)
+    resized_ict = ict_min_logo.resize((new_width*2-10, new_height-10),resample)
     # Convert to Tkinter PhotoImage
-    new_photo = ImageTk.PhotoImage(resized)
+    new_photo_bau = ImageTk.PhotoImage(resized_bau)
+    new_photo_ict = ImageTk.PhotoImage(resized_ict)
 
     # Update the label with the resized image
-    label.config(image=new_photo)
-    label.image = new_photo
+    bau_logo_label.config(image=new_photo_bau)
+    bau_logo_label.image = new_photo_bau
+    ict_min_logo_label.config(image=new_photo_ict)
+    ict_min_logo_label.image = new_photo_ict
 # main run function
 def run():
     # Color palate declaration
@@ -25,11 +31,11 @@ def run():
     app = tk.Tk()
 
     # Image load
+    global logo,bau_logo,ict_min_logo
     logo = tk.PhotoImage(file='images//demo_logo.png')
-    bau_logo = tk.PhotoImage(file='images//bau_logo.png')
-    ict_min_logo = tk.PhotoImage(file='images//ict_min_logo.png')
-
-    # Set icon & title
+    bau_logo =  Image.open('images//bau_logo.png')
+    ict_min_logo = Image.open('images//ict_min_logo.png')
+   
     app.iconphoto(False, logo)
     app.title("DNA SEQUENCE SIMILARITY ANALYZER")
 
@@ -48,8 +54,16 @@ def run():
     head.place(relx=0,rely=0,relheight=0.1,relwidth=1)
 
     # Head contents
-    bau_logo_label=tk.Label(head,image=bau_logo)
-    bau_logo_label.place(relx=0.1,rely=0.1,relheight=0.8)
+    bau_logo_tk = ImageTk.PhotoImage(bau_logo)
+    global bau_logo_label
+    bau_logo_label=tk.Label(head,image=bau_logo_tk,bg=col_w)
+    bau_logo_label.place(relx=0.05,rely=0.05,relheight=0.9)
+    
+    ict_min_logo_tk = ImageTk.PhotoImage(ict_min_logo)
+    global ict_min_logo_label
+    ict_min_logo_label=tk.Label(head,image=ict_min_logo_tk,bg=col_w)
+    ict_min_logo_label.place(relx=0.85,rely=0.05,relheight=0.9)
+    head.bind('<Configure>', resize_image)
 
     app.mainloop()
 
