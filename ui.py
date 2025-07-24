ab0e8aeae1ff831530ac31e84861c76bc5cbfbb0
Ishmam <mdishmam2003@gmail.com>
2025-07-24 21:27:31 +0600

    method 1 implimentation and ui update

--- ui.py
+++ ui.py
@@ -2,12 +2,15 @@ import sys
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QWidget, QFrame, QLabel, QPushButton,
     QComboBox, QListWidget, QVBoxLayout, QHBoxLayout, QToolButton,
-    QLineEdit, QFileDialog, QRadioButton, QSpinBox, QMenu
+    QLineEdit, QFileDialog, QRadioButton, QButtonGroup, QSpinBox, QMenu,QMessageBox
 )
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap,QIcon
 from qt_material import apply_stylesheet
-
+from matplotlib import pyplot as plt
+import method1
+import os
+from Bio import Phylo
 
 class BAUSimilarityWindow(QMainWindow):
     def __init__(self):
@@ -108,6 +111,7 @@ class PhyloTreeWindow(QMainWindow):
         self.setWindowTitle("Phylogenetic Tree")
         self.setWindowIcon(QIcon("images/demo_logo.png"))
         self.resize(900, 600)
+        self.move(100,50)
         
         central = QWidget()
         self.setCentralWidget(central)
@@ -147,7 +151,6 @@ class PhyloTreeWindow(QMainWindow):
 
         # Browse Fasta
         browse_h = QHBoxLayout()
-        browse_h.addWidget(QLabel("Browse Fasta"))
         self.path_edit = QLineEdit()
         self.path_edit.setStyleSheet("width:200px;")
         self.path_edit.setReadOnly(True)
@@ -155,7 +158,9 @@ class PhyloTreeWindow(QMainWindow):
         browse_btn = QPushButton("Browse...")
         browse_btn.clicked.connect(self.browse_fasta)
         browse_h.addWidget(browse_btn)
-        browse_h.addWidget(QLabel("Count:"))
+        cnt_lbl=QLabel("Count:")
+        cnt_lbl.setStyleSheet("border:none;""background-color:transparent")
+        browse_h.addWidget(cnt_lbl)
         self.count_label = QLabel("0")
         browse_h.addWidget(self.count_label)
         browse_h.addStretch()
@@ -163,15 +168,20 @@ class PhyloTreeWindow(QMainWindow):
 
         # Select k
         select_h = QHBoxLayout()
-        select_h.addWidget(QLabel("Select k"))
+        select_lbl=QLabel("Select k : ")
+        select_lbl.setStyleSheet("border:none;""background-color:transparent")
+        select_h.addWidget(select_lbl)
         self.rb_default = QRadioButton("Default")
         self.rb_default.setChecked(True)
         self.rb_custom = QRadioButton("Custom")
         self.spin_k = QSpinBox()
-        self.spin_k.setRange(1, 100)
+        self.spin_k.setRange(1, 10)
+        self.spin_k.setValue(4)
         self.spin_k.setEnabled(False)
         self.rb_default.toggled.connect(lambda checked: self.spin_k.setEnabled(not checked))
-
+        self.button_k=QButtonGroup(self)
+        self.button_k.addButton(self.rb_custom)
+        self.button_k.addButton( self.rb_default)
         select_h.addWidget(self.rb_default)
         select_h.addWidget(self.rb_custom)
         select_h.addWidget(self.spin_k)
@@ -182,32 +192,46 @@ class PhyloTreeWindow(QMainWindow):
 
         # Options button
         opts_h = QHBoxLayout()
-        opts_btn = QToolButton()
-        opts_btn.setText("Options")
-        opts_btn.setPopupMode(QToolButton.ToolButtonPopupMode.MenuButtonPopup)
-        opts_menu = QMenu()
-        opts_btn.setStyleSheet("width:100px;")
-        opts_menu.addAction("Option 1")
-        opts_menu.addAction("Option 2")
-        opts_btn.setMenu(opts_menu)
-        opts_h.addWidget(opts_btn)
+        draw_btn = QPushButton("Generate Phylogenetic tree")
+        draw_btn.clicked.connect(self.draw_phylo)
+        #draw_btn.setStyleSheet("Width:50px;")
+        opts_h.addWidget(draw_btn)
         opts_h.addStretch()
+        opts_lbl=QLabel("Select Thresh percent (%) : ")
+        opts_lbl.setStyleSheet("border:none;""background-color:transparent")
+        opts_h.addWidget(opts_lbl)
+        self.t_df_default = QRadioButton("Default")
+        self.t_df_default.setChecked(True)
+        self.t_cm_custom = QRadioButton("Custom")
+        self.t_spin = QSpinBox()
+        self.t_spin.setRange(0, 100)
+        self.t_spin.setValue(50)
+        self.t_spin.setEnabled(False)
+        self.t_df_default.toggled.connect(lambda checked: self.t_spin.setEnabled(not checked))
+        self.button_t=QButtonGroup(self)
+        self.button_t.addButton( self.t_df_default)
+        self.button_t.addButton(self.t_cm_custom)
+        opts_h.addWidget(self.t_df_default)
+        opts_h.addWidget(self.t_cm_custom)
+        opts_h.addWidget(self.t_spin)
+        
         layout.addLayout(opts_h)
 
         # Tree frame
         tree_frame = QFrame()
         tree_frame.setFrameShape(QFrame.Shape.Box)
         tf_l = QVBoxLayout(tree_frame)
-        lbl = QLabel("Phylogenetic Tree")
-        lbl.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        tf_l.addWidget(lbl)
-        layout.addWidget(tree_frame, stretch=4)
+        self.lbl = QLabel("Phylogenetic Tree")
+        self.lbl.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        tf_l.addWidget(self.lbl)
+        layout.addWidget(tree_frame, stretch=1)
 
         # Bottom row
         bottom_h2 = QHBoxLayout()
         bottom_h2.addStretch(1)
-        bottom_h2.addWidget(QLabel("Box"))
-        bottom_h2.addWidget(QPushButton("Rotate"))
+        self.download=QPushButton("Download")
+        self.download.clicked.connect(self.save_tree)
+        bottom_h2.addWidget(self.download)
         
         layout.addLayout(bottom_h2)
         
@@ -219,25 +243,75 @@ class PhyloTreeWindow(QMainWindow):
         n_lbl.setAlignment(Qt.AlignmentFlag.AlignCenter)
         ntf_l.addWidget(n_lbl)
         layout.addWidget(newick_tree_frame, stretch=1)
+        self.fname=None
 
     def browse_fasta(self):
-        fname, _ = QFileDialog.getOpenFileName(
-            self, "Open FASTA", "", "FASTA Files (*.fa *.fasta);;All Files (*)"
+        self.fname = QFileDialog.getExistingDirectory(
+    self, "Select Folder", "", QFileDialog.Option.ShowDirsOnly
         )
-        if not fname:
+        if not self.fname:
             return
-        self.path_edit.setText(fname)
+        self.path_edit.setText(self.fname)
         count = 0
         try:
-            with open(fname) as f:
-                for line in f:
-                    if line.startswith(">"):
-                        count += 1
+            for file in os.listdir(self.fname):
+            # check only text files
+                if file.endswith('.fasta'):
+                    count+=1
         except Exception:
             count = 0
         self.count_label.setText(str(count))
+    
+    def draw_phylo(self):
+        if self.fname==None:
+            QMessageBox.critical(self,"Error","Select File First")
+        else:
+            try:
+                dtm = method1.DynamicTM
+                
+                if self.rb_default.isChecked():
+                    k_len=4
+                else:
+                    k_len=self.spin_k.value()
+                
+                if self.t_df_default.isChecked():
+                    thresh=50
+                else:
+                    thresh=self.t_spin.value()
+                    
+                tree_rcv = dtm.read_fas1(self.fname,k_len,thresh,self=None)
 
-
+                current_directory = os.getcwd()
+                fig = plt.figure(figsize=(16,10), dpi=100)
+                axes = fig.add_subplot(1, 1, 1)
+                Phylo.draw(tree_rcv, axes=axes, do_show=False)
+                fig_name = current_directory + '/phylogenetic_tree/' + 'phylo.png'
+                #plt.savefig('v5.png')
+                plt.savefig(fig_name)
+                plt.show()
+                plt.close()
+                phylo_tree_path = 'phylogenetic_tree/phylo.png'
+                lbl_pix=QPixmap(phylo_tree_path)
+                self.real_img=lbl_pix
+                lbl_pix=lbl_pix.scaled(800,500, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+                self.lbl.setPixmap(lbl_pix)
+                # Set the file pointer to the beginning of the stream
+            except Exception as e:
+                print(e)
+            
+    def save_tree(self):
+        try:
+            if self.real_img:
+                save_path, _ = QFileDialog.getSaveFileName(
+                    self, "Save Image", "", "PNG Files (*.png);;JPEG Files (*.jpg);;All Files (*)"
+                )
+                if save_path:
+                    self.real_img.save(save_path)
+                else:
+                    QMessageBox.critical(self,"Error","Error try again")
+        except:
+            QMessageBox.critical(self,"Error","Generate Tree first")
+       
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     apply_stylesheet(app, theme='light_blue.xml')
@@ -245,6 +319,6 @@ if __name__ == "__main__":
         app.setStyleSheet(file.read())
     bau = BAUSimilarityWindow()
     phylo = PhyloTreeWindow()
-    bau.show()
+    #bau.show()
     phylo.show()
     sys.exit(app.exec())
