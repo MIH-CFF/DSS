68ebb49e826a9749e1d31a8f68dc0f35c9c32c94
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-10-09 13:45:06 +0600

    [ux]: disabled non required routes;

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -9,20 +9,20 @@ import { ConfigProvider } from 'antd';
 import enUS from 'antd/locale/en_US';
 
 import { CustomLayout } from './layout/CustomLayout';
-import { LazyLanding } from './component/landing/LazyLanding';
-import { LazyPage2 } from './component/LazyPage2';
-import { LazyPage3 } from './component/LazyPage3';
+// import { LazyLanding } from './component/landing/LazyLanding';
+// import { LazyPage2 } from './component/LazyPage2';
+// import { LazyPage3 } from './component/LazyPage3';
 import { DSAAnalysis } from './component/DSAAnalysis';
 
 const App = () => (
   <ConfigProvider locale={enUS}>
-    <BrowserRouter basename="/react-pipeline">
+    <BrowserRouter basename="/dss">
       <CustomLayout>
         <Routes>
-          <Route path="/landing" element={<LazyLanding />} />
+          {/* <Route path="/landing" element={<LazyLanding />} /> */}
           <Route path="/analysis" element={<DSAAnalysis />} />
-          <Route path="/page2" element={<LazyPage2 />} />
-          <Route path="/page3" element={<LazyPage3 />} />
+          {/* <Route path="/page2" element={<LazyPage2 />} /> */}
+          {/* <Route path="/page3" element={<LazyPage3 />} /> */}
           <Route
             path="*"
             element={<Navigate to="/analysis" replace />}
