b70b951e2fdaf02a96e8b49883da612cb9068491
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-10-09 13:36:23 +0600

    [feat: add API package for REST backend functionality;

---
+++ src/api/__init__.py
@@ -0,0 +1,13 @@
+"""
+API package for REST backend functionality
+"""
+
+# Import will be available once the modules exist
+try:
+    from .app import create_app
+    from .models import *
+    __all__ = ['create_app']
+except ImportError:
+    # During initial import, modules might not be fully loaded
+    # todo: handle this more gracefully, low priority
+    __all__ = []
